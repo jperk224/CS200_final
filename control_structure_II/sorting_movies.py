435d4046b322ef14cb6e4f9319834a054f7f0c67
Jeff <jmperkinson@gmail.com>
2019-06-12 14:24:03 -0400

    almost there, bug with print by director spacing

--- control_structure_II/sorting_movies.py
+++ control_structure_II/sorting_movies.py
@@ -5,7 +5,17 @@
 
 movie_collection = {
     2005: [['Munich', 'Steven Spielberg']],
-    2006: [['The Prestige', 'Christopher Nolan'], ['The Departed', 'Martin Scorsese']]
+    2006: [['The Prestige', 'Christopher Nolan'], ['The Departed', 'Martin Scorsese']],
+    2007: [['Into the Wild', 'Sean Penn']],
+    2008: [['The Dark Knight', 'Christopher Nolan']],
+    2009: [['Mary and Max', 'Adam Elliot']],
+    2010: [['The King\'s Speech', 'Tom Hooper']],
+    2011: [['The Artist', 'Michel Hazanavicius'], ['The Help', 'Tate Taylor']],
+    2012: [['Argo', 'Ben Affleck']],
+    2013: [['12 Years a Slave', 'Steve McQueen']],
+    2014: [['Birdman', 'Alejandro G. Inarritu']],
+    2015: [['Spotlight', 'Tom McCarthy']],
+    2016: [['The BFG', 'Steven Spielberg']]
 }
 
 # Prompt the user for a single year and output the movie title(s) and director(s) from that year. Output N/A if the year is invalid.
@@ -13,7 +23,7 @@ movie_collection = {
 # initialize user's year selection and max and min key values
 min_year = min(list(movie_collection.keys()))
 max_year = max(list(movie_collection.keys()))
-user_year = int(input('Enter a year between 2005 and 2006: \n'))
+user_year = int(input('Enter a year between 2005 and 2016:\n'))
 # while (user_year < min(list(movie_collection.keys()))) and (user_year > max(list(movie_collection.keys()))):
 #     user_year = int(input('Enter a year between '))
 
@@ -22,6 +32,7 @@ if (user_year < min_year) or (user_year > max_year):
 else:
     for film in movie_collection[user_year]:
         print('%s, %s' % (film[0], film[1]))
+print()
 
 # Display a menu that enables a user to display the movies sorted by year, director, or movie title.
 # Each option is represented by a single character.
@@ -32,9 +43,9 @@ options_menu = 'MENU\n' \
                'Sort by:\n' \
                'y - Year\n' \
                'd - Director\n' \
-               't - Movie Title\n' \
-               'q - Quit'
-print()
+               't - Movie title\n' \
+               'q - Quit\n'
+# print()
 print(options_menu)
 
 # initialize loop scope variables
@@ -42,9 +53,9 @@ year_list = list(movie_collection.keys())
 movie_list = list(movie_collection.values())
 movie_tuples = list(movie_collection.items())
 
-user_option = input('Choose an option: \n').lower().strip()
+user_option = input('Choose an option:\n').lower().strip()
 while user_option not in ['y', 'd', 't', 'q']:
-    user_option = input('Choose an option: \n').lower().strip()
+    user_option = input('Choose an option:\n').lower().strip()
 while user_option != 'q':
     # Implement the sort by year menu option
     # Year:
@@ -54,6 +65,8 @@ while user_option != 'q':
             print('%d:' % year)
             for film in movie_collection[year]:
                 print('\t%s, %s' % (film[0], film[1]))
+            print()
+
     # Implement the sort by director menu option
     # For directors with multiple films on the list, order their films by year.
     # Director:
@@ -73,6 +86,8 @@ while user_option != 'q':
                 for film in movie_collection[year]:
                     if (film[1] == director):
                         print('\t%s, %d' % (film[0], year))
+            print()
+
     # implement the sort by title menu option
     # Title:
     #    Director, Year
@@ -93,4 +108,6 @@ while user_option != 'q':
                 for film in movie_collection[year]:
                     if (film[0] == title):
                         print('\t%s, %d' % (film[1], year))
-    user_option = input('Choose an option: \n').lower().strip()
+            print()
+    print(options_menu)
+    user_option = input('Choose an option:\n').lower().strip()
