a011f02d347bed7394881b86be5484f87917c9e2
Jeff <jmperkinson@gmail.com>
2019-06-16 14:58:05 -0400

    Clean up comments for more context/clarification.
    The assignment calls for annotated code to explain the following:
    * Choice of data structure
    * Algorithm reasoning
    * Use of best practices

--- control_structure_I/half-arrow.py
+++ control_structure_I/half-arrow.py
@@ -1,16 +1,15 @@
-# This program outputs a downwards facing half arrow composed of a rectangle and a right triangle
+# This program outputs a downwards facing half arrow composed of a rectangle and a right triangle.
 # The arrow dimensions are defined by user specified 
 # arrow base height, arrow base width, and arrow head width.
 
 # Function Definitions
-# Three funcitons are defined
-# Draw the base of the arrow
-# Draw the head of the arrow
-# Draw the half arrow
-# Separate definitions are defined for clarity
-# Each function serves a separate purpose 
-# For loops are used for drawing as we know the number of iterations
-# based on the int arguments passed in
+# Three funcitons are defined.
+# Draw the base of the arrow.
+# Draw the head of the arrow.
+# Draw the half arrow.
+# Separate definitions are defined for clarity.
+# Each function serves a separate, single purpose .
+# For loops are used for drawing as the parameters dictate the number of iterations.
 
 def draw_base(height:int, width:int) -> str:
     '''
@@ -20,7 +19,7 @@ def draw_base(height:int, width:int) -> str:
     height -- an int representing the desired height
     width -- an int representing the desired width
     '''
-    # Draw arrow base (height = base_height, width = base_width)
+    # Draw arrow base (height = base_height, width = base_width).
     for i in range(height):
         print('*'*width)
 
@@ -31,7 +30,7 @@ def draw_head(head_width:int) -> str:
     Keyword arguments:
     head_width -- an int representing the desired head width
     '''
-    # Draw arrow head (head_width = largest width at head base)
+    # Draw arrow head (head_width = largest width at head base).
     for i in reversed(range(1, head_width + 1)):
         print('*'*i)
 
@@ -44,26 +43,27 @@ def draw_arrow(height:int, width:int, head_width:int) -> str:
     width -- an int representing the desired width
     head_width -- an int representing the desired head width
     '''
-    # Draw half arrow (arrow base + arrow head)
+    # Draw half arrow (arrow base + arrow head).
     draw_base(height, width)    
     draw_head(head_width)
 
 # Only execute if this is the main script run 
 # and script is not imported by another module
 if __name__ == "__main__":
-    # Grab user defined dimenstions and assign them to variables
-    # these variables will serve as arguments for specific instances
-    # of the functions defined above
+    # Grab user defined dimenstions and assign them to variables.
+    # These variables will serve as arguments for specific instances
+    # of the functions defined above.
 
-    # Converting the string intput into int data types is best here for later iteration
-    # The loops defined in the functions will not iterate over strings or floats as currently defined
+    # Converting the string intput into int data types is needed for function iteration.
+    # The loops defined in the functions will not iterate over strings or floats as currently defined.
 
     base_height = int(input('Enter arrow base height: \n'))
     base_width = int(input('Enter arrow base width: \n'))
     head_width = int(input('Enter arrow head width: \n'))
 
-    # A while loop is used to collect input to ensure 
-    # head width is greater than base width to ensure arrow shape
+    # A while loop is used to collect input to ensure the
+    # head width is greater than base width to ensure arrow shape.
+    # A while loop is used because the number of iterations is unknown.
 
     while (head_width <= base_width):
         head_width = int(input('Head width must be greater than base width\nEnter arrow head width: \n'))
